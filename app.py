72b6d6860380f10051bc92905298acb6abbf26eb
k-song14 <93162546+k-song14@users.noreply.github.com>
2022-05-28 20:39:08 -0700

    changes

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ flask run
 
 '''
 
-from flask import Flask, render_template, request
+from flask import Flask, g, render_template, request
 from flask import redirect, url_for, abort
+import numpy as np
+import sqlite3
 
 app = Flask(__name__)
 
@@ -16,16 +18,66 @@ def base():
     return render_template("base.html")
 
 @app.route("/submit/", methods=['POST', 'GET'])
-def ask():
+def submit():
     if request.method == 'GET':
         # if the user just visits the url
         return render_template('submit.html')
     else:
-        # if the user submits the form
-        message = request.form['message']
-        name = request.form['name']
-        return render_template('submit.html', message=message, name=name)
+        try:
+            # if the user submits the form
+            message = request.form['message']
+            name = request.form['name']
+            # call the database function if successful submission
+            return render_template('submit.html', thanks=True, message=message, name=name)
+        except:
+            return render_template('submit.html', error=True)
 
-@app.route("/view/")
+def get_message_db():
+    try:
+        return g.message_db
+    except:
+        g.message_db = sqlite3.connect("messages_db.sqlite")
+        cmd = \
+        '''
+        CREATE TABLE IF NOT EXISTS `messages` (
+            id INTEGER PRIMARY KEY AUTOINCREMENT  ,
+            name TEXT NOT NULL,
+            message TEXT NOT NULL
+        );
+        '''
+        cursor = g.message_db.cursor()
+        cursor.execute(cmd)
+        return g.message_db
+
+def insert_message(request):
+    conn = get_message_db()
+    cmd = \
+    f'''
+    INSERT INTO messages
+    VALUES (name, message);
+    '''
+    cursor = conn.cursor()
+    cursor.execute(cmd)
+    conn.commit()
+    conn.close()
+
+@app.route('/view/')
 def view():
-    return render_template("view.html")
+
+    #render template
+    #return render_template('view.html', messages = random_messages(5))
+    db = get_message_db()
+
+    #SQL command
+    messages = db.execute("SELECT message FROM messages").fetchall()
+    return render_template('view.html', messages = messages)
+
+#def random_messages(n):
+    # refer to insert_messages and discussion view function 
+    # HINT SQL command - ORDER BY RANDOM()
+
+    #connect database
+    db = get_message_db()
+
+    #SQL command
+    messages = db.execute("SELECT * FROM").fetchall()
